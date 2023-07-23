4286556b890dec66623b935a92729b0e4b1c293f
Fidelium <20m02.k@gmail.com>
2023-07-23 20:21:31 +0200

    Steuerung eingebunden

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -26,68 +26,30 @@ export default function GameScreen({ }) {
   const [projectionPoint, setProjectionPoint] = useState(null);
 
 
-
-  const [tiltDirection, setTiltDirection] = useState("none");
-
-  useEffect(() => { // implement accelerometer
-    Accelerometer.setUpdateInterval(100);
-    Accelerometer.addListener((accelerometerData) => {
-      const { x, y, z } = accelerometerData;
-
-      // Calculate the pitch and roll angles in radians
-    const pitch = Math.atan2(x, Math.sqrt(y * y + z * z));
-    const roll = Math.atan2(y, Math.sqrt(x * x + z * z));
-
-    // Convert radians to degrees
-    const pitchDegrees = (pitch * 180) / Math.PI;
-    const rollDegrees = (roll * 180) / Math.PI;
-
-    // Normalize values to be within 0-360 degrees
-    const pitchNormalized = (pitchDegrees + 360) % 360;
-    const rollNormalized = (rollDegrees + 360) % 360;
-
-    // Output the tilt direction
-    let tiltDirectionX = "None";
-    let tiltDirectionY = "None";
-
-    if (pitchNormalized >= 45 && pitchNormalized <= 135) {
-      tiltDirectionX = "Down";
-    } else if (pitchNormalized >= 225 && pitchNormalized <= 315) {
-      tiltDirectionX = "Up";
-    }
-
-    if (rollNormalized >= 45 && rollNormalized <= 135) {
-      tiltDirectionY = "Left";
-    } else if (rollNormalized >= 225 && rollNormalized <= 315) {
-      tiltDirectionY = "Right";
-    }
-
-    // console.log(`Tilt X: ${Math.round(pitchNormalized)}° (${tiltDirectionX})`);
-    //console.log(`Tilt Y: ${Math.round(rollNormalized)}° (${tiltDirectionY})`);
-    });
-  }, []);
-
-
-
-  // console.log tilt direction every 1000ms
   useEffect(() => {
-    const interval = setInterval(() => {
-      // console.log(tiltDirection);
-      if (tiltDirection === "left") {
-        moveBall({ x: -5, y: 0 });
-      }
-      if (tiltDirection === "right") {
-        moveBall({ x: 5, y: 0 });
-      }
-      if (tiltDirection === "down") {
-        moveBall({ x: 0, y: 5 });
-      }
-      if (tiltDirection === "up") {
-        moveBall({ x: 0, y: -5 });
-      }
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [tiltDirection]);
+    let subscription;
+    const subscribeToAccelerometer = async () => {
+        Accelerometer.setUpdateInterval(16); // Set the update interval (in milliseconds)
+        subscription = Accelerometer.addListener(accelerometerData => {
+            const { x, y } = accelerometerData;
+            const tiltThreshold = 0.01; // Adjust this threshold based on your need
+
+            if (Math.abs(x) > tiltThreshold || Math.abs(y) > tiltThreshold) {
+                moveBall({ x: Math.round(-x * 10), y: Math.round(-y * 15) });
+            }
+        });
+    };
+
+    subscribeToAccelerometer();
+
+    // Clean up the subscription when the component unmounts
+
+    return () => {
+        if (subscription) {
+            subscription.remove();
+        }
+    };
+}, []);
 
   const checkCollision = useCallback((potentialPosition) => {
     let closestObstaclePoints = [];
@@ -147,20 +109,25 @@ export default function GameScreen({ }) {
   
   
   // simple function to move the ball by using similar logic as in the useEffect above
-  const moveBall = useCallback(
-    (direction) => {
-      const { x, y } = direction;
-      const newBallPosition = { x: ballPosition.x + x, y: ballPosition.y + y };
-   
-  
-      if(checkCollision(newBallPosition)){
-        // revert ball's position if there is a collision
-      } else {
-        setBallPosition(newBallPosition);
-      }
-    },
-    [ballPosition, checkCollision]
-  );
+  const moveBall = useCallback(({ x, y }) => {
+    setBallPosition(prevPosition => {
+        const nextPosition = {
+            x: prevPosition.x + x,
+            y: prevPosition.y + y,
+        };
+
+        if (checkCollision(nextPosition)) {
+            console.log('collision detected');
+
+            // Perform collision response with useCallback function handleCollision
+            const newPosition = prevPosition;
+
+            return newPosition;
+        }
+        return nextPosition;
+    });
+});
+    
   
 
 
@@ -216,7 +183,6 @@ export default function GameScreen({ }) {
 
     </Svg>
         <Text style={styles.positionText}>
-          tiltDirection: {tiltDirection},
         ballX: {ballPosition.x}, ballY: {ballPosition.y}
       </Text>
       <View style={styles.buttonsContainer}>
@@ -231,16 +197,16 @@ export default function GameScreen({ }) {
         />
         <View style={styles.moveButtons}>
           <View style={styles.leftButton}>
-            <Button title="←" onPress={() => moveBall({ x: -1, y: 0 })} />
+            <Button title="←" onPress={() => moveBall({ x: -10, y: 0 })} />
           </View>
           <View style={styles.rightButton}>
-            <Button title="→" onPress={() => moveBall({ x: 1, y: 0 })} />
+            <Button title="→" onPress={() => moveBall({ x: 10, y: 0 })} />
           </View>
           <View style={styles.upButton}>
-            <Button title="↑" onPress={() => moveBall({ x: 0, y: -1 })} />
+            <Button title="↑" onPress={() => moveBall({ x: 0, y: -10 })} />
           </View>
           <View style={styles.downButton}>
-            <Button title="↓" onPress={() => moveBall({ x: 0, y: 1 })} />
+            <Button title="↓" onPress={() => moveBall({ x: 0, y: 10 })} />
           </View>
         </View>
       </View>
