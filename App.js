0a7c94086964c49e9a3db7ed4fc32d7ac7f9a795
M4T1SS3 <markorester@gmail.com>
2023-07-23 18:41:31 +0200

    collision detection almost finished

--- App.js
+++ App.js
@@ -1,30 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
-import * as db from './logic/database.js';
-import 'react-native-gesture-handler'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import MyStack from './logic/StackNavigator.js';
+import { Text, View } from 'react-native';
+import GameScreen from './screens/GameScreen';
+import { SettingsProvider } from './context/SettingsContext';
 
-/*import MenuScreen from './screens/MenuScreen.js';
-import GameScreen from './screens/GameScreen.js';
-import GameOverScreen from './screens/GameOverScreen.js';
-import GameWinScreen from './screens/GameWinScreen.js';
-import GameLoseScreen from './screens/GameLoseScreen.js';
-import GamePauseScreen from './screens/GamePauseScreen.js';
-import GameResumeScreen from './screens/GameResumeScreen.js';
-import GameRestartScreen from './screens/GameRestartScreen.js';
-import GameQuitScreen from './screens/GameQuitScreen.js';*/
-
-const Stack = createStackNavigator();
 
 export default function App() {
-  db.init();
 
   return (
-    <NavigationContainer>
-      <MyStack />
-    </NavigationContainer>
+    <SettingsProvider>
+     <GameScreen/>
+    </SettingsProvider>
   );
 };
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,14 +1,14 @@
 // Date: 2021/05/30
 import React, { useState, useEffect, useContext, useCallback } from "react";
 import { Button, StyleSheet, Text, View, Dimensions } from "react-native";
-import Svg from "react-native-svg";
 import { Accelerometer } from "expo-sensors";
-import { SettingsContext } from "../context/SettingsContext";
 import { Obstacle, Line, Ball } from "../components/gameElements";
 import { GameStyles } from "../styles/GameStyles";
 import SettingsScreen from "./SettingsModalScreen";
+import { SettingsContext } from "../context/SettingsContext";
+import { Circle, Svg, Polygon } from "react-native-svg";
 
-export default function GameScreen({ navigation }) {
+export default function GameScreen({ }) {
   const styles = GameStyles;
   const { ballSize, setBallSize } = useContext(SettingsContext);
   const [ballPosition, setBallPosition] = useState({ x: 50, y: 100 });
@@ -20,10 +20,11 @@ export default function GameScreen({ navigation }) {
 
   const obstacleCorners = [
     { id: 1, points: ["100,50", "200,50", "200,150", "100,150"] },
+    { id: 3, points: ["100,250", "200,250", "200,350", "100,350", "400,320"] },
   ];
+  const [closestPoints, setClosestPoints] = useState([]);
 
-  // q: how to get the tilt direction?
-  // a: use accelerometer
+
   const [tiltDirection, setTiltDirection] = useState("none");
 
   useEffect(() => { // implement accelerometer
@@ -59,7 +60,7 @@ export default function GameScreen({ navigation }) {
       tiltDirectionY = "Right";
     }
 
-    console.log(`Tilt X: ${Math.round(pitchNormalized)}° (${tiltDirectionX})`);
+    // console.log(`Tilt X: ${Math.round(pitchNormalized)}° (${tiltDirectionX})`);
     //console.log(`Tilt Y: ${Math.round(rollNormalized)}° (${tiltDirectionY})`);
     });
   }, []);
@@ -69,7 +70,7 @@ export default function GameScreen({ navigation }) {
   // console.log tilt direction every 1000ms
   useEffect(() => {
     const interval = setInterval(() => {
-      console.log(tiltDirection);
+      // console.log(tiltDirection);
       if (tiltDirection === "left") {
         moveBall({ x: -5, y: 0 });
       }
@@ -86,14 +87,70 @@ export default function GameScreen({ navigation }) {
     return () => clearInterval(interval);
   }, [tiltDirection]);
 
+  const checkCollision = useCallback(() => {
+    let closestObstaclePoints = [];
+    let minObstacleDistance = Infinity;
+  
+    for(let obstacle of obstacleCorners){
+      const points = obstacle.points.map(point => point.split(',').map(Number));
+      let pointsWithDistances = [];
+  
+      for(let point of points) {
+        const distance = Math.sqrt(Math.pow(ballPosition.x - point[0], 2) + Math.pow(ballPosition.y - point[1], 2));
+        pointsWithDistances.push({ point, distance });
+      }
+  
+      pointsWithDistances.sort((a, b) => a.distance - b.distance);
+      
+      if (pointsWithDistances[0].distance < minObstacleDistance) {
+        minObstacleDistance = pointsWithDistances[0].distance;
+        closestObstaclePoints = pointsWithDistances.slice(0, 2).map(pd => pd.point);
+      }
+    }
+  
+    setClosestPoints(closestObstaclePoints);
+  
+    let vec1 = { x: closestObstaclePoints[0][0] - ballPosition.x, y: closestObstaclePoints[0][1] - ballPosition.y };
+    let vec2 = { x: closestObstaclePoints[1][0] - ballPosition.x, y: closestObstaclePoints[1][1] - ballPosition.y };
+  
+    let angle = Math.acos(
+      (vec1.x * vec2.x + vec1.y * vec2.y) /
+      (Math.sqrt(vec1.x * vec1.x + vec1.y * vec1.y) * Math.sqrt(vec2.x * vec2.x + vec2.y * vec2.y))
+      );
+    
+    let angleInDegrees = angle * (180 / Math.PI);
+    let oppositeAngleInDegrees = 180 - angleInDegrees;
+  
+    console.log("Closest Points:", closestObstaclePoints);
+    console.log("Angle in degrees:", angleInDegrees);
+    console.log("Opposite Angle in degrees:", oppositeAngleInDegrees);
+    const collisionDegree = 130;
+   
+  
+    // if(angleInDegrees >= collisionDegree || minObstacleDistance <= ballRadius) {
+    //   return true; // collision
+    // }
+  
+    return false; // no collision
+  }, [ballPosition, ballRadius]);
+  
+  
   // simple function to move the ball by using similar logic as in the useEffect above
   const moveBall = useCallback(
     (direction) => {
       const { x, y } = direction;
-      setBallPosition({ x: ballPosition.x + x, y: ballPosition.y + y });
+      const newBallPosition = { x: ballPosition.x + x, y: ballPosition.y + y };
+      setBallPosition(newBallPosition);
+  
+      if(checkCollision()){
+        // revert ball's position if there is a collision
+        setBallPosition(ballPosition);
+      }
     },
-    [ballPosition]
+    [ballPosition, checkCollision]
   );
+  
+
 
 
   // implement joystick to move the ball
@@ -109,7 +166,23 @@ export default function GameScreen({ navigation }) {
           </React.Fragment>
         );
       })}
+        {closestPoints.map((point, index) => (
+      <Circle
+        key={index}
+        cx={point[0]} // x-coordinate
+        cy={point[1]} // y-coordinate
+        r={5} // radius
+        fill="red" // color
+      />
+    ))}
+    {closestPoints.length === 2 && (
+          <Polygon
+            points={`${ballPosition.x},${ballPosition.y} ${closestPoints[0][0]},${closestPoints[0][1]} ${closestPoints[1][0]},${closestPoints[1][1]}`}
+            fill="pink"
+          />
+        )}
       <Ball x={ballPosition.x} y={ballPosition.y} ballSize={ballSize} />
+      
     </Svg>
         <Text style={styles.positionText}>
           tiltDirection: {tiltDirection},
